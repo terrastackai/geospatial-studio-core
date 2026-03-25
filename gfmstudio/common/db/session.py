7739a8714386cf19d5891cddbf0586aa32119115
Fiona-Bundi <Fiona.Bundi@ibm.com>
2026-03-25 12:52:16 +0300

    🐛 fix: add pool preping and error handling to avoid broken pipe error

--- gfmstudio/common/db/session.py
+++ gfmstudio/common/db/session.py
@@ -2,10 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+from multiprocessing import pool
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from ...config import settings
 
-engine = create_engine(str(settings.DATABASE_URI), pool_size=30, max_overflow=10)
+engine = create_engine(str(settings.DATABASE_URI), pool_size=30, max_overflow=10, pool_pre_ping=True,pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
